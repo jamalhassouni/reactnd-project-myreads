fe4d63dcf952dd6259a195ef6254d0b1664c6c1d
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-19 10:22:13 +0100

    Add Shelf component to display the books in their shelf

---
+++ src/Shelf.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Book from "./Book";
+
+const Shelf = props => {
+  return (
+    <div className="bookshelf" key={props.keys}>
+      <h2
+        className="bookshelf-title"
+        style={{ width: "100%" }}
+      >
+        {props.shelfName}
+      </h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.isLoading && (
+            <div>
+              <li className="loader" />
+              <h1 className="loadText">Content is Loading</h1>
+            </div>
+          )}
+          {!props.isLoading && props.books.length !== 0 ? (
+            props.books.map(book => {
+              return (
+                <li key={book.id}>
+                  <Book book={book} onChangeShelf={props.onChangeShelf} />
+                </li>
+              );
+            })
+          ) : (
+            <li className="message">There are no books in this shelf</li>
+          )}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Shelf;
