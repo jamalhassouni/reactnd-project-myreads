700cfc72eb0fd3f38c29263c809ac83566ca76ba
jamal hassouni <jamalhassouni13@gmail.com>
2018-08-19 11:51:42 +0100

    feat:Add Page404 component to display message when no page has the same URL as the user search for

--- src/Page404.js
+++ src/Page404.js
@@ -7,9 +7,9 @@ const Page404 = () => {
             <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-            <div style={{ width: '100%', textAlign: "center"  }} >
-            <h1 style={{ fontSize: '80pt', marginBottom: '0px'}}>404</h1>
-            <h3 style={{ fontSize: '25pt'}}>There is no Page with this url.</h3>
+            <div className="page-error">
+            <h1>404</h1>
+            <h3>There is no Page with this url.</h3>
               <Link to="/" className="redirectHome">Return to Home Page</Link>
             </div>
         </div>
